79b961358b1015147ff99e053f8938b7a969eaed
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-08-05 12:29:26 +0530

    Added smooth transition from navbar links and added styling to scrollbar

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,13 +1,12 @@
-import Link from "next/link";
 
-const NavLink = ({ href, title }) => {
+
+const NavLink = ({ title }) => {
   return (
-    <Link
-      href={href}
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+    <div
+      className="block cursor-pointer py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
     >
       {title}
-    </Link>
+    </div>
   );
 };
 
